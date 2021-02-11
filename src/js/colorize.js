dd49532d6d1eb448bb91bb3fc8997ae036fa0ea8
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-02-11 17:40:55 +0200

    Рефакторинг

--- src/js/colorize.js
+++ src/js/colorize.js
@@ -7,28 +7,29 @@ const stopBtnRef = document.querySelector('button[data-action="stop"]');
 startBtnRef.addEventListener('click', changeBgInInterval);
 stopBtnRef.addEventListener('click', stopChangeBg);
 
+const INTERVAL_DELAY = 1000;
 let intervalId = null;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const getRandomColor = function (colors, randomIndex) {
-  const randomColorIndex = randomIndex(0, colors.length - 1);
+function getRandomColor() {
+  const randomColorIndex = randomIntegerFromInterval(0, colors.length - 1);
 
   return colors[randomColorIndex];
-};
+}
 
-function colorizePage() {
-  const randomColor = getRandomColor(colors, randomIntegerFromInterval);
-
-  bodyRef.style.backgroundColor = randomColor;
+function setPageBg() {
+  bodyRef.style.backgroundColor = getRandomColor();
 }
 
 function changeBgInInterval() {
-  intervalId = setInterval(colorizePage, 1000);
+  intervalId = setInterval(setPageBg, INTERVAL_DELAY);
+  startBtnRef.disabled = true;
 }
 
 function stopChangeBg() {
   clearInterval(intervalId);
+  startBtnRef.disabled = false;
 }
